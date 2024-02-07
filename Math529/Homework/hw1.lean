118da436b5550bfd9aa04f623c7c40947ff50237
Sergey Cherkis <cherkis@gmail.com>
2024-02-07 13:55:00 -0700

    HW01

--- Math529/Homework/hw1.lean
+++ Math529/Homework/hw1.lean
@@ -1,29 +1,17 @@
-import Mathlib.Data.Real.Basic
+import Mathlib.Tactic
 import AutograderLib
+--import Mathlib.Data.Real.Basic
 
 
 /-! # Homework 1
+-/
 
-Don't forget to compare with the text version,
-for clearer statements and any special instructions. -/
+@[autograded 5]
+theorem problem1 {a b : ℝ} : (∀ ε > 0, a < b + ε) → a ≤ b := by
+sorry
+
 
 
 @[autograded 5]
-theorem problem1 {p q : ℤ} (h1 : p + 4 * q = 1) (h2 : q - 1 = 2) : p = -11 := by
-  sorry
-
-@[autograded 5]
-theorem problem2 {a b : ℝ} (h1 : a + 2 * b = 4) (h2 : a - b = 1) : a = 2 := by
-  sorry
-
-@[autograded 5]
-theorem problem3 {x : ℤ} (hx : x ≥ 9) : x ^ 3 - 8 * x ^ 2 + 2 * x ≥ 3 := by
-  sorry
-
-@[autograded 5]
-theorem problem4 {x : ℚ} : x ^ 2 - 2 * x ≥ -1 := by
-  sorry
-
-@[autograded 5]
-theorem problem5 (a b : ℝ) (h1 : -b ≤ a) (h2 : a ≤ b) : a ^ 2 ≤ b ^ 2 := by
-  sorry
+theorem problem2 {p q r : Prop} : p ∧ ¬ (q ∨ r) ↔ (p ∧ ¬ q) ∧ (p ∧ ¬ r) := by
+sorry
