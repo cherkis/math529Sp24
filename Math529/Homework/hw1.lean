39c2bfedc59ccfdd96cf955cf161adb3a3c50fcf
Sergey Cherkis <cherkis@gmail.com>
2024-02-08 08:54:37 -0700

    HW1 added comments

--- Math529/Homework/hw1.lean
+++ Math529/Homework/hw1.lean
@@ -2,6 +2,10 @@ import Mathlib.Tactic
 import AutograderLib
 
 /-! # Homework 1
+Write your proofs in convernitonal (on paper) form for each of the problems first.  Then fill in the LEAN proofs below, replacing `sorry`.
+
+You can comment out `import AutograderLib`, while you are writing your solution.
+Do not forget to uncomment it, before you save and submit your solution to Gradescope.
 -/
 
 @[autograded 5]
