22011dc9bfb26c0dd4a436d5a53f5112d10718e0
Sergey Cherkis <cherkis@gmail.com>
2026-02-04 11:45:46 -0700

    Added Homework 1

---
+++ Math469T/hw01.lean
@@ -0,0 +1,68 @@
+import AutograderLib
+import Mathlib.Tactic
+
+/-! # Homework 1
+To begin with let prove the basic properties of ∧, ∨, and ¬.
+-/
+
+section
+
+variable (P Q R : Prop)
+
+-- Commutativity
+@[autograded 5]
+theorem problem1 : P ∧ Q ↔ Q ∧ P := by
+  sorry
+  done
+
+@[autograded 5]
+theorem problem2 : P ∨ Q ↔ Q ∨ P := by
+  sorry
+  done
+
+ -- Associativity
+@[autograded 5]
+theorem problem3 : (P ∧ Q) ∧ R ↔ Q ∧ (P ∧ R) := by
+  sorry
+  done
+
+@[autograded 5]
+theorem problem4 : (P ∨ Q) ∨ R ↔ Q ∨ (P ∨ R) := by
+  sorry
+  done
+
+-- Distributivity of ∧ over ∨
+@[autograded 5]
+theorem problem5 : P ∧ (Q ∨ R) ↔ (P ∧ Q) ∨ (P ∧ R) := by
+  sorry
+  done
+
+-- Distributivity of ∨ over ∧
+@[autograded 5]
+theorem problem6 : P ∨ (Q ∧ R) ↔ (P ∨ Q) ∧ (P ∨ R) := by
+  sorry
+  done
+
+@[autograded 5]
+theorem problem7 : ¬ (P → Q) ↔ P ∧ ¬ Q := by
+  sorry
+  done
+
+@[autograded 5]
+theorem problem8 : ∀ S : Prop, ¬ P → (P → S) := by
+  sorry
+  done
+
+-- This might require some logical thinking first
+@[autograded 5]
+theorem problem9 : ∃ Q, ∀ P, P ∨ Q ↔ Q := by
+  sorry
+  done
+
+@[autograded 5]
+theorem problem10 : ∃ Q, ∀ P, P ∨ Q ↔ P := by
+  sorry
+  done
+
+
+end
