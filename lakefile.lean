2acebbcbed19bec591c047a9f373422f9e007cc7
cherkis <67087578+cherkis@users.noreply.github.com>
2026-02-05 15:47:06 -0700

    Rename package from Math529SP24 to math529SP24

--- lakefile.lean
+++ lakefile.lean
@@ -1,7 +1,7 @@
 import Lake
 open Lake DSL
 
-package Math529SP24 where
+package math529SP24 where
   moreServerOptions := #[
     ⟨`linter.unusedVariables, false⟩, -- ignores unused variables
     ⟨`quotPrecheck, false⟩,
